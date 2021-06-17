ef17a76cd6998f7262cebe6e879c9be23db379c2
Akira-Ito0109 <syobonnito@gmail.com>
2021-06-17 16:31:09 +0900

    NejikoRun終了

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -14,10 +15,27 @@ public class GameController : MonoBehaviour
         int score = CalcScore();
         scoreText.text = "Score:" + score + "m";
         lifePanel.UpdateLife(nejiko.Life());
+
+        if(nejiko.Life()<=0)
+        {
+            enabled = false;
+
+            if(PlayerPrefs.GetInt("HighScore")<score)
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+
+            Invoke("ReturnToTitle", 2.0f);
+        }
     }
 
     int CalcScore()
     {
         return (int)nejiko.transform.position.z;
+    }
+
+    void ReturnToTitle()
+    {
+        SceneManager.LoadScene("Title");   
     }
 }
